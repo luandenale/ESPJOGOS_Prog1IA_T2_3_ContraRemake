5f16633f09bd0c1210fcd000ccaf9c4a0842a18b
Luan De Nale <19193111@pucrs.br>
2019-06-21 22:22:30 -0300

    Added camera follow behaviour.

---
+++ T2-3_Contra_Remake/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private float _initialPos = -67.31f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(PlayerManager.instance.PlayerDirection.x > 0f && PlayerManager.instance.transform.position.x > _initialPos)
+        {
+            if (PlayerManager.instance.transform.position.x > transform.position.x)
+                transform.position = new Vector3(PlayerManager.instance.transform.position.x, transform.position.y, transform.position.z);
+        }
+
+    }
+}
