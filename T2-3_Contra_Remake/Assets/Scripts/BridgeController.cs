4042060659d5c61a49282f02971202a238855f51
Luan De Nale <luandenale@hotmail.com>
2019-07-06 21:02:36 -0300

    Finished adding bridge.

---
+++ T2-3_Contra_Remake/Assets/Scripts/BridgeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeController : MonoBehaviour
+{
+    [SerializeField] float playerXPosTrigger;
+    [SerializeField] Animator explosionsAnimator;
+
+    private Animator _bridgeAnimator;
+
+    private void Awake()
+    {
+        _bridgeAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(PlayerManager.instance.transform.position.x > playerXPosTrigger)
+        {
+            _bridgeAnimator.SetTrigger("Destroy");
+            explosionsAnimator.SetTrigger("Explode");
+        }
+    }
+
+}
